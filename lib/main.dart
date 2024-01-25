5b29c5e536cb5ffb1d869d2df3e68f1410264005
littleboyaba <northlightingstar@gmail.com>
2024-01-26 01:31:12 +0600

    Widget => Text and  some of its properties.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -20,13 +21,26 @@ class HomeActivity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('Flutter Widgets'),
-      ),
-      body: const Center(
-        child: Text("Hello Flutter"),
+    return const Scaffold(
+      //backgroundColor: Color(0xFF338930),
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Text(
+          "Hello Flutter, How are you doing? Hope you are doing well? Yeah! about that, things are doing upside down",
+          textAlign: TextAlign.justify,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.teal,
+            decoration: TextDecoration.underline,
+            wordSpacing: 3,
+            letterSpacing: 2,
+            height:2,
+          ),
+        ),
       ),
     );
   }
