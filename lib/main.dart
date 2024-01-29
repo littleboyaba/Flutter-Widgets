4caad6bd663c5866f8e90279d2fe4c0c654ee3e8
littleboyaba <northlightingstar@gmail.com>
2024-01-29 23:52:37 +0600

    Widget => RichText

--- lib/main.dart
+++ lib/main.dart
@@ -24,42 +24,51 @@ class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppBar'),
-        backgroundColor: Colors.blue,
-        elevation: 3,
-        leading: const Icon(
-          Icons.home,
-          color: Colors.white,
-          size: 28,
-        ),
-      ),
-      //backgroundColor: Color(0xFF338930),
-      body: Center(
-        child: RichText(
-          text: TextSpan(
-            text: 'Flutter ',
-            style: TextStyle(color: Colors.purple, fontSize: 24),
-            children: const [
-              TextSpan(
-                text: 'Rich ',
-                style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
-              ),
-              TextSpan(
-                text: 'Text',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 20,
-                  decoration: TextDecoration.lineThrough,
-                ),
-              )
-            ],
+        appBar: AppBar(
+          title: const Text('AppBar'),
+          backgroundColor: Colors.blue,
+          elevation: 3,
+          leading: const Icon(
+            Icons.home,
+            color: Colors.white,
+            size: 28,
           ),
         ),
-      ),
-    );
+        //backgroundColor: Color(0xFF338930),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print('Tapped on Hello Button');
+                },
+                onLongPress: () {
+                  print('You have long pressed Hello Button');
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey.shade400,
+                    foregroundColor: Colors.black54,
+                    // maximumSize: Size(300, 80),
+                    // minimumSize: Size(250, 40),
+                    // padding: EdgeInsets.all(16),
+                    // padding: EdgeInsets.symmetric(vertical: 12, horizontal: 32),
+                    padding: EdgeInsets.only(left: 12, top: 24, right: 36, bottom: 48),
+                    elevation: 4,
+                    shadowColor: Colors.teal,
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white, // color won't work for "foregroundColor" as its set to black.
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+                child: Text('Hello'),
+              ),
+
+            ],
+          ),
+        ));
   }
 }
