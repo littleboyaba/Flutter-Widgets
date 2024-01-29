b8a3ae7da08f346a6e798f5f225cdeb1459179e8
littleboyaba <northlightingstar@gmail.com>
2024-01-30 01:57:53 +0600

    Widget => TextField

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Flutter Widget",
       home: HomeActivity(),
     );
@@ -19,7 +19,11 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+  HomeActivity({super.key});
+  // TextEditingController use to control textField
+  final TextEditingController nameTextFieldController = TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,59 +43,60 @@ class HomeActivity extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
-              onPressed: () {
-                print('Pressed on Text Button');
+            TextField(
+              controller: nameTextFieldController,
+              onTap: () {
+                print('TEXT FIELD TAPPED');
               },
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.teal.shade300,
-                  foregroundColor: Colors.white,
-                  // maximumSize: Size(300, 50),
-                  // minimumSize: Size(250, 40),
-                  padding: EdgeInsets.all(20),
-                  // padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                  // padding: EdgeInsets.only(left: 12, top: 20, right: 12, bottom: 20),
-                  elevation: 3,
-                  shadowColor: Colors.blueGrey,
-                  textStyle: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                    color: Colors
-                        .red, // text color wont work here as foregroundColor set on above.
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  )),
-              child: Text("Text Button"),
-            ),
-            SizedBox(height: 20,),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
-              style: IconButton.styleFrom(
-                  backgroundColor: Colors.green.shade200,
-                  foregroundColor: Colors.white70,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  )),
-            ),
-            SizedBox(height: 20,),
-            FloatingActionButton(
-              onPressed: () {},
-              child: Text("FAB Button"),
-            ),
-            SizedBox(height: 20,),
-            FloatingActionButton.extended(
-              onPressed: () {},
-              label: Text("FAB Button"),
+              // you can set TextInputType in keyboardType.
+              keyboardType: TextInputType.text,
+              // onChanged used to validate text input. you can see the out put in console.
+              onChanged: (String input) {
+                print(input);
+              },
+              // textInputAction change keyboard Action Button.
+              textInputAction: TextInputAction.done,
+
+              // TextEditingController use case example.
+              onSubmitted: (String value){
+                print(nameTextFieldController.text);
+                nameTextFieldController.clear();
+              },
+
+              // use to hind text when user input it. like for password or other uses.
+              // obscureText: ture,
+              obscureText: false,
+
+              // Decoration
+              decoration: InputDecoration(
+                enabled:
+                    true, // use this to enable or disable your text field. it can be use in directly in TextField too.
+                label: Text("Name"),
+                hintText: 'Enter your name',
+
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.green, width: 5, style: BorderStyle.solid),
+                ),
+
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.purple, width: 5, style: BorderStyle.solid),
+                ),
+
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.teal, width: 5, style: BorderStyle.solid),
+                ),
+
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.red, width: 5, style: BorderStyle.solid),
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
       ),
     );
   }
